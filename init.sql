202764321c21d94c263c50d85b3623c412db1c11
Carlos Eduardo Azevedo <30722229+CarlosEduard0@users.noreply.github.com>
2024-02-25 19:32:05 -0500

    feat: add nginx and postgres configurations

---
+++ init.sql
@@ -0,0 +1,23 @@
+drop table if exists "Transactions";
+drop table if exists "Clients";
+
+create table "Clients" (
+    "Id" int primary key,
+    "Limit" int not null,
+    "Balance" int not null
+);
+
+insert into "Clients" values
+    (1, 100000, 0),
+    (2, 80000, 0),
+    (3, 1000000, 0),
+    (4, 10000000, 0),
+    (5, 500000, 0);
+
+create unlogged table "Transactions" (
+    "Amount" int not null,
+    "OperationType" char(1) not null,
+    "Description" varchar(10) not null,
+    "CreatedAt" timestamp with time zone not null,
+    "ClientId" int not null
+);
